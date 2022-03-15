6efa808c8f0575ffc67e34967489a6319c513224
wenbo.hu <hubogle@gmail.com>
2022-03-15 21:41:04 +0800

    add: 买卖股票的最佳时机 II

--- editor/cn/53最大子数组和.go
+++ editor/cn/53最大子数组和.go
@@ -52,46 +52,21 @@ func main() {
 
 // leetcode submit region begin(Prohibit modification and deletion)
 func maxSubArray(nums []int) int {
-	// ans := nums[0] // 默认最大为第一个
-	// for i := 1; i < len(nums); i++ {
-	// 	nums[i] = max(nums[i], nums[i]+nums[i-1])
-	// 	ans = max(ans, nums[i])
-	// }
-	// return ans
-	pre, ans := nums[0], nums[0]
+	ans := nums[0]
 	for i := 1; i < len(nums); i++ {
-		pre = max(nums[i], pre+nums[i])
-		ans = max(pre, ans)
+		if nums[i-1] > 0 {
+			nums[i] += nums[i-1]
+		}
+		if nums[i] > ans {
+			ans = nums[i]
+		}
 	}
 	return ans
-}
-
-func max(a, b int) int {
-	if a > b {
-		return a
-	}
-	return b
 }
 
 // leetcode submit region end(Prohibit modification and deletion)
 
 func maxSubArray1(nums []int) int {
-	dp := make([]int, len(nums))
-	dp[0] = nums[0]
-	ans := nums[0] // 默认最大为第一个
-	for i := 1; i < len(nums); i++ {
-		if dp[i-1] > 0 { // 如果前面的值大于 0
-			dp[i] = dp[i-1] + nums[i]
-		} else { // 前面的值小于 0 就不用累加
-			dp[i] = nums[i]
-		}
-		if dp[i] > ans {
-			ans = dp[i]
-		}
-	}
-	return ans
-}
-func maxSubArray2(nums []int) int {
 	dp := make([]int, len(nums))
 	dp[0] = nums[0]
 	ans := nums[0] // 默认最大为第一个
@@ -106,23 +81,51 @@ func maxSubArray2(nums []int) int {
 	}
 	return ans
 }
-func maxSubArray3(nums []int) int {
+
+func maxSubArray4(nums []int) int {
 	ans := nums[0] // 默认最大为第一个
 	for i := 1; i < len(nums); i++ {
-		if nums[i-1] > 0 {
-			nums[i] += nums[i-1]
+		nums[i] = max(nums[i], nums[i]+nums[i-1])
+		ans = max(ans, nums[i])
+	}
+	return ans
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// 贪心解法
+func maxSubArray5(nums []int) int {
+	count := 0
+	ans := ^int(^uint(0) >> 1)
+	for _, v := range nums {
+		count += v
+		if count > ans {
+			ans = count
 		}
-		if nums[i] > ans {
-			ans = nums[i]
+		if count < 0 {
+			count = 0
 		}
 	}
 	return ans
 }
 
 /*
-题目思路：dp 数组，存储每个索引位置 i 当前的最大值。
+题目思路：动态规划
+dp 数组，存储每个索引位置 i 当前的最大值。
 情况1：dp[i-1] > 0 则 dp[i] = dp[i-1] + nums[i]
 情况2：dp[i-1] < 0 则 dp[i] = nums[i]
-分治法就是分别获取左右最大值，然后两边最大值进行相加
 
+题目思路：分治法
+就是分别获取左右最大值，然后两边最大值进行相加
+
+题目思路：贪心思想
+设置一个最大值，和一个当前计算值。
+循环不断累加值，当遇到的值小于 0 时就重置累加值为0，从新开始累加。
+局部最优：当连续和为负数时，从下一个元素重新计算元素和。
+全局最优：选取最大连续和
 */
