057c4134a0cf4722370ceb0f575c2ecf9a16bd8f
wenbo.hu <hubogle@gmail.com>
2022-03-18 23:54:59 +0800

    add: 相交链表

---
+++ editor/cn/215数组中的第K个最大元素.go
@@ -0,0 +1,43 @@
+//给定整数数组 nums 和整数 k，请返回数组中第 k 个最大的元素。 
+//
+// 请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: [3,2,1,5,6,4] 和 k = 2
+//输出: 5
+// 
+//
+// 示例 2: 
+//
+// 
+//输入: [3,2,3,1,2,4,5,5,6] 和 k = 4
+//输出: 4 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= k <= nums.length <= 10⁴ 
+// -10⁴ <= nums[i] <= 10⁴ 
+// 
+// Related Topics 数组 分治 快速选择 排序 堆（优先队列） 👍 1540 👎 0
+
+
+package main
+import "fmt"
+
+func main(){
+    result := kthLargestElementInAnArray()
+    fmt.Println(result)
+}
+
+//leetcode submit region begin(Prohibit modification and deletion)
+func findKthLargest(nums []int, k int) int {
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
